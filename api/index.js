f2b441518769fa96213a1d94d374fb2122a587e9
oeyoews <jyao4783@gmail.com>
2023-03-13 13:48:55 +0800

    refactor: use api

--- api/index.js
+++ api/index.js
@@ -1,26 +1,30 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
 
 const quotes = [
-  "The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela",
-  "The way to get started is to quit talking and begin doing. -Walt Disney",
-  "Your time is limited, don't waste it living someone else's life. -Steve Jobs",
-  "If life were predictable it would cease to be life, and be without flavor. -Eleanor Roosevelt",
-  "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. -Oprah Winfrey",
-  "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. -James Cameron",
-  "Life is what happens when you're busy making other plans. -John Lennon",
-  "Spread love everywhere you go. Let no one ever come to you without leaving happier. -Mother Teresa",
-  "When you reach the end of your rope, tie a knot in it and hang on. -Franklin D. Roosevelt",
-  "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart. -Helen Keller",
+  {
+    text: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+  },
+  {
+    text: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  },
+  {
+    text: "Your time is limited, don't waste it living someone else's life.",
+    author: "Steve Jobs",
+  },
 ];
 
-app.use(cors());
-
+app.get("/api", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const quote = quotes[randomIndex];
+  res.json(quote);
+});
 app.get("/", (req, res) => {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const quote = quotes[randomIndex];
-  res.send(quote);
+  res.json(quote);
 });
 
 module.exports = app;
