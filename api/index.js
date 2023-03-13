f815859853de9b1a5eda94fa784654f04865e4c8
oeyoews <jyao4783@gmail.com>
2023-03-13 14:59:51 +0800

    fix: add missing require cors

--- api/index.js
+++ api/index.js
@@ -1,20 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const path = require("path");
 const fs = require("fs");
 
 const quotes = [
   {
-    text: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
-    author: "Nelson Mandela",
-  },
-  {
-    text: "The way to get started is to quit talking and begin doing.",
-    author: "Walt Disney",
-  },
-  {
-    text: "Your time is limited, don't waste it living someone else's life.",
-    author: "Steve Jobs",
+    text: "Success 🎉 ",
+    author: "@oeyoews",
   },
 ];
 
