e4b34b2e8251077a66a2d011f038dc9d760ddecd
oeyoews <jyao4783@gmail.com>
2023-03-13 15:44:31 +0800

    test: trye to support param

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const quotes = [
 app.use(cors());
 
 // json file
-app.get("/api", (req, res) => {
+app.get("/api/:id?", (req, res) => {
   const filePath = path.join(__dirname, "assets", "quotes.json");
   fs.readFile(filePath, (err, data) => {
     if (err) {
@@ -23,13 +23,31 @@ app.get("/api", (req, res) => {
       return;
     }
     const quotes = JSON.parse(data);
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    const quote = quotes[randomIndex];
-    res.json(quote);
+    let quote;
+    if (req.params.id === "htk" || req.query.select === "#htk") {
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      quote = quotes[randomIndex];
+      const html = `<html><body><div id="htk">Hello, this is the default quote.</div></body></html>`;
+      const dom = new DOMParser().parseFromString(html, "text/html");
+      const htkElement = dom.querySelector("#htk");
+      htkElement.textContent = quote.text;
+      res.send(dom.documentElement.outerHTML);
+    } else {
+      const selectedQuote = req.query.select;
+      if (selectedQuote) {
+        quote = quotes.find((q) => q.text === selectedQuote);
+      } else {
+        const randomIndex = Math.floor(Math.random() * quotes.length);
+        quote = quotes[randomIndex];
+      }
+      const encodedQuote =
+        req.query.encode === "js" ? JSON.stringify(quote) : quote;
+      res.json(encodedQuote);
+    }
   });
 });
 
-// example
+// test
 app.get("/", (req, res) => {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const quote = quotes[randomIndex];
