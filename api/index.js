323bd357abf6350f298c72702e0cc8914800ce30
oeyoews <jyao4783@gmail.com>
2023-05-23 19:25:07 +0800

    Automatic commit

--- api/index.js
+++ api/index.js
@@ -1,8 +1,3 @@
-/* import express from "express";
-import cors from "cors";
-import path from "path";
-import fs from "fs"; */
-
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
