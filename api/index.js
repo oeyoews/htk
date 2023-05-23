91450fa17768176664785c78fa0759d406678993
oeyoews <jyao4783@gmail.com>
2023-05-23 19:03:41 +0800

    fix: not use modules

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
-import express from "express";
+/* import express from "express";
 import cors from "cors";
 import path from "path";
-import fs from "fs";
+import fs from "fs"; */
+
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-import axios from "axios";
+/* import axios from "axios";
 import gradient from "gradient-string";
-import chalk from "chalk";
+import chalk from "chalk"; */
 
-export const Test = async () => {
+const axios = require("axios");
+const gradient = require("gradient-string");
+// const { chalk } = require("chalk");
+
+const Test = async () => {
   axios.get("https://htk.vercel.app/api").then((response) => {
     const data = response.data;
     const colors = ["#12c2e9", "#c471ed"];
 
     const gradientString = gradient(colors)(`${data.text} @${data.author}`);
-    console.log(chalk.bgGray(gradientString));
+    console.log(gradientString);
+    // console.log(chalk.bgGray(gradientString));
   });
 };
 
